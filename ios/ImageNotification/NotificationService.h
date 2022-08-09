825b10b243f288b9e04193d8ee94a9ea91586d31
Jake Landers <jake@landersweb.com>
2022-08-09 00:05:33 -0700

    images in notifications, auto load on scroll in chat, image detail in chat

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Jake Landers on 8/8/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
