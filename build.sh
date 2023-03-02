7b5b0463783b55d4ff5eeb72bffff4fa2805773f
Jake Landers <jake@landersweb.com>
2023-03-02 08:02:41 -0800

    working with tagging system

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@
 shaderFile="./shader_master.json"
 
 # build ios
-flutter build ipa --enable-impeller --bundle-sksl-path $shaderFile
+# flutter build ipa --bundle-sksl-path $shaderFile
+flutter build ipa
 
 # build android
-flutter build appbundle --bundle-sksl-path $shaderFile
+# flutter build appbundle --bundle-sksl-path $shaderFile
+flutter build appbundle 
